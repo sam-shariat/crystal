cd8cadea3ad58f6a3e7cacfbd9186515c598626a
Sam Shariat <samshariat7@gmail.com>
2023-06-10 06:37:20 +0330

    fixing types

--- components/sections/ClaimSection.tsx
+++ components/sections/ClaimSection.tsx
@@ -26,7 +26,7 @@ import { useMediaQuery, useColorMode } from '@chakra-ui/react';
 import { VENOMSCAN_NFT, SITE_PROFILE_URL, SITE_MANAGE_URL } from 'core/utils/constants';
 
 interface Message {
-  type: string | undefined;
+  type: "info" | "error" | "success" | "warning" | undefined;
   title: string;
   msg: string;
   link?: string;
