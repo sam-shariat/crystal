ec78a7d48abf136ce4f0f2ee4477ce30b6f82947
Sam Shariat <samshariat7@gmail.com>
2023-06-14 03:14:56 +0330

    little bug for mint

--- components/sections/ClaimSection.tsx
+++ components/sections/ClaimSection.tsx
@@ -257,7 +257,7 @@ export default function ClaimSection() {
               minWidth="300px"
               disabled={name.length < 3 || nameExists}
               isLoading={feeIsLoading || isMinting}
-              onClick={(e) => claimVid(e.target.disabled)}>
+              onClick={(e) => claimVid(e.target.value)}>
               {t('claimButton')}
             </Button>
           </Stack>
