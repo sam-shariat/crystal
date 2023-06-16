58850b5c55d67576de2090b6d78500eae764c3f7
Sam Shariat <samshariat7@gmail.com>
2023-06-17 00:04:14 +0330

    fixing

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    outputStandalone: true,
-  }
+  reactStrictMode: true
 };
 
 module.exports = nextConfig;
