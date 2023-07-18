46a67e3ece865c701eca96fb9f6eda5ce70e6dcf
Sam Shariat <samshariat7@gmail.com>
2023-07-19 02:52:47 +0330

    added live on testnet notice

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -2,6 +2,7 @@ import {
   Box,
   Button,
   Container,
+  Center,
   Flex,
   HStack,
   Stack,
@@ -19,7 +20,7 @@ import {
   DrawerContent,
   DrawerHeader,
   DrawerBody,
-  useDisclosure
+  useDisclosure,
 } from '@chakra-ui/react';
 import NextLink from 'next/link';
 import { localeAtom, colorModeAtom } from 'core/atoms';
@@ -28,84 +29,87 @@ import { useAtom } from 'jotai';
 import Logo from './Logo';
 import { RiMoonFill, RiSunFill, RiMenu2Fill, RiCloseFill } from 'react-icons/ri';
 import { Locale } from 'translations';
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import { useTranslate } from 'core/lib/hooks/use-translate';
 
 export default function Header() {
-  const [colorM,setColorM] = useAtom(colorModeAtom);
+  const [colorM, setColorM] = useAtom(colorModeAtom);
   const { colorMode, toggleColorMode } = useColorMode();
   const [locale, setLocale] = useAtom(localeAtom);
   const [notMobile] = useMediaQuery('(min-width: 800px)');
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const { pathname } = useRouter()
+  const { pathname } = useRouter();
   const { t } = useTranslate();
   const home = pathname === '/' ? true : false;
 
-  useEffect(()=> {
-    console.log(colorMode + " is " + colorM)
-    if(colorMode !== colorM){
-      
+  useEffect(() => {
+    console.log(colorMode + ' is ' + colorM);
+    if (colorMode !== colorM) {
       toggleColorMode();
     }
-  },[colorMode])
+  }, [colorMode]);
 
   return (
     <Box
       as="nav"
       borderBottom="1px"
       backgroundColor={useColorModeValue('blackAlpha.100', 'blackAlpha.500')}
-      borderBottomColor={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
-      py={2}>
-      <Container maxW="container.lg">
+      borderBottomColor={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}>
+      <Center px={4} py={2} bgColor="var(--venom2)">
+        VenomID is now live on Venom Testnet !
+      </Center>
+      <Container maxW="container.lg" py={2}>
         <Flex justifyContent="space-between">
           <HStack>
             {!notMobile && (
-              <IconButton aria-label='venomid-mobile-menu' variant="ghost" onClick={onOpen}>
+              <IconButton aria-label="venomid-mobile-menu" variant="ghost" onClick={onOpen}>
                 <RiMenu2Fill />
               </IconButton>
             )}
             <NextLink href="/" passHref>
-              <Button id="venomidlogo" color="var(--venom1)" fontWeight="bold" variant="ghost" px={2}>
+              <Button
+                id="venomidlogo"
+                color="var(--venom1)"
+                fontWeight="bold"
+                variant="ghost"
+                px={2}>
                 <Logo />
                 <Text pl={1}>VenomID</Text>
               </Button>
             </NextLink>
-            {notMobile && home &&(
+            {notMobile && home && (
               <NextLink href="#manage" passHref>
-                <Button variant="ghost">
-                  {t('manage')}
-                </Button>
+                <Button variant="ghost">{t('manage')}</Button>
               </NextLink>
             )}
             {notMobile && home && (
               <NextLink href="#profile" passHref>
-                <Button variant="ghost">
-                  {t('profile')}
-                </Button>
+                <Button variant="ghost">{t('profile')}</Button>
               </NextLink>
             )}
             {notMobile && home && (
               <NextLink href="#roadmap" passHref>
-                <Button variant="ghost">
-                  {t('roadmap')}
-                </Button>
+                <Button variant="ghost">{t('roadmap')}</Button>
               </NextLink>
             )}
             {notMobile && home && (
               <NextLink href="#about" passHref>
-                <Button variant="ghost">
-                  {t('about')}
-                </Button>
+                <Button variant="ghost">{t('about')}</Button>
               </NextLink>
             )}
           </HStack>
           <HStack dir="ltr">
             <ConnectButton />
-            {notMobile && home &&(
+            {notMobile && home && (
               <Menu>
                 <MenuButton as={Button}>{locale.toUpperCase()}</MenuButton>
-                <MenuList py={0} width={100} border={1} borderColor={'grey'} bg={colorMode === 'light' ? 'var(--lightGrey)': 'var(--darkGradient)'}>
+                <MenuList
+                  py={0}
+                  width={100}
+                  border={1}
+                  borderColor={'grey'}
+                  bg={colorMode === 'light' ? 'var(--lightGrey)' : 'var(--darkGradient)'}>
                   <MenuItem onClick={() => setLocale(Locale.En)}>EN</MenuItem>
                   <MenuItem onClick={() => setLocale(Locale.Fa)}>فا</MenuItem>
                 </MenuList>
@@ -114,8 +118,10 @@ export default function Header() {
             {notMobile && (
               <IconButton
                 aria-label="theme"
-                onClick={()=> {setColorM(colorMode === 'light' ? 'dark' : 'light'); toggleColorMode()} }
-                
+                onClick={() => {
+                  setColorM(colorMode === 'light' ? 'dark' : 'light');
+                  toggleColorMode();
+                }}
                 icon={colorMode === 'light' ? <RiMoonFill /> : <RiSunFill />}
               />
             )}
@@ -140,56 +146,62 @@ export default function Header() {
               <IconButton
                 variant="ghost"
                 aria-label="theme"
-                onClick={()=> {setColorM(colorMode === 'light' ? 'dark' : 'light'); toggleColorMode()} }
-                
+                onClick={() => {
+                  setColorM(colorMode === 'light' ? 'dark' : 'light');
+                  toggleColorMode();
+                }}
                 icon={colorMode === 'light' ? <RiMoonFill /> : <RiSunFill />}
               />
-              <IconButton aria-label='closemenu' variant="ghost" onClick={onClose}>
+              <IconButton aria-label="closemenu" variant="ghost" onClick={onClose}>
                 <RiCloseFill />
               </IconButton>
             </HStack>
           </DrawerHeader>
           <DrawerBody>
-            {home && <Stack py={4}>
-              <NextLink href="#manage" passHref>
-                <Button variant="ghost" width="100%" justifyContent="left">
-                  {t('manage')}
+            {home && (
+              <Stack py={4}>
+                <NextLink href="#manage" passHref>
+                  <Button variant="ghost" width="100%" justifyContent="left">
+                    {t('manage')}
+                  </Button>
+                </NextLink>
+                <NextLink href="#profile" passHref>
+                  <Button variant="ghost" width="100%" justifyContent="left">
+                    {t('profile')}
+                  </Button>
+                </NextLink>
+                <NextLink href="#roadmap" passHref>
+                  <Button variant="ghost" width="100%" justifyContent="left">
+                    {t('roadmap')}
+                  </Button>
+                </NextLink>
+                <NextLink href="#about" passHref>
+                  <Button variant="ghost" width="100%" justifyContent="left">
+                    {t('about')}
+                  </Button>
+                </NextLink>
+              </Stack>
+            )}
+            {home && (
+              <Stack borderTopWidth="1px" width="100%" py={4}>
+                <Button
+                  onClick={() => setLocale(Locale.En)}
+                  fontWeight="bold"
+                  variant="ghost"
+                  width="100%"
+                  justifyContent="left">
+                  English
                 </Button>
-              </NextLink>
-              <NextLink href="#profile" passHref>
-                <Button variant="ghost" width="100%" justifyContent="left">
-                {t('profile')}
+                <Button
+                  onClick={() => setLocale(Locale.Fa)}
+                  fontWeight="bold"
+                  variant="ghost"
+                  width="100%"
+                  justifyContent="left">
+                  فارسی
                 </Button>
-              </NextLink>
-              <NextLink href="#roadmap" passHref>
-                <Button variant="ghost" width="100%" justifyContent="left">
-                {t('roadmap')}
-                </Button>
-              </NextLink>
-              <NextLink href="#about" passHref>
-                <Button variant="ghost" width="100%" justifyContent="left">
-                {t('about')}
-                </Button>
-              </NextLink>
-            </Stack>}
-            {home && <Stack borderTopWidth="1px" width="100%" py={4}>
-              <Button
-                onClick={() => setLocale(Locale.En)}
-                fontWeight="bold"
-                variant="ghost"
-                width="100%"
-                justifyContent="left">
-                English
-              </Button>
-              <Button
-                onClick={() => setLocale(Locale.Fa)}
-                fontWeight="bold"
-                variant="ghost"
-                width="100%"
-                justifyContent="left">
-                فارسی
-              </Button>
-            </Stack>}
+              </Stack>
+            )}
           </DrawerBody>
         </DrawerContent>
       </Drawer>
