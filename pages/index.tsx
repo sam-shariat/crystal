030ac7dca711870454d6dd1e8b4f3831aac24f27
Sam Shariat <samshariat7@gmail.com>
2023-07-21 00:02:36 +0330

    fixing twitter seo

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,16 +7,22 @@ import RoadmapSection from 'components/sections/RoadmapSection';
 import AboutSection from 'components/sections/AboutSection';
 import { SITE_DESCRIPTION, SITE_FULL_DESCRIPTION, SITE_TITLE } from 'core/utils/constants';
 import { Seo } from 'components/Layout/Seo';
+import { NextSeo } from 'next-seo';
 
 const Home: NextPage = () => {
   return (
     <>
       <Seo />
       <Head>
-        <title>{SITE_TITLE} | {SITE_DESCRIPTION}</title>
+        <title>
+          {SITE_TITLE} | {SITE_DESCRIPTION}
+        </title>
         <meta name="description" content={SITE_FULL_DESCRIPTION} />
-        <link rel="icon" type="image/svg+xml" href="/logos/vidicon.svg"/>
-        <link rel="icon" type="image/png" href="/logos/vidicon.png"/>
+        <meta name="twitter:title" content={SITE_TITLE + ' | ' + SITE_DESCRIPTION} />
+        <meta name="twitter:description" content={SITE_FULL_DESCRIPTION} />
+        <meta name="twitter:image" content="/vidorigin.png" />
+        <link rel="icon" type="image/svg+xml" href="/logos/vidicon.svg" />
+        <link rel="icon" type="image/png" href="/logos/vidicon.png" />
       </Head>
 
       <ClaimSection />
